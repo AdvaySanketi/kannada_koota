bef1184ec691161cc7636a03e64691cdf2d570ba
Advay Sanketi <advay2807@gmail.com>
2024-08-15 20:41:46 +0530

    testing something

--- src/lib/mongo.js
+++ src/lib/mongo.js
@@ -1,6 +1,9 @@
 import { MongoClient } from "mongodb";
 
-const client = new MongoClient(process.env.MONGO_URI);
+// const client = new MongoClient(process.env.MONGO_URI);
+const client = new MongoClient(
+  "mongodb+srv://advay2807:q4BG7aIfbUwHd5uR@kannada-koota.zogoa.mongodb.net/?retryWrites=true&w=majority&appName=Kannada-Koota"
+);
 
 let clientPromise;
 
